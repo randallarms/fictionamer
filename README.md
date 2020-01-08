7cb2f77a11fd96d06f713e1182775890244f9772
Randall Arms <contact.randallArms@gmail.com>
2020-01-08 06:12:55 -0800

    Initial commit
    * Initial commit.

---
+++ README.md
@@ -0,0 +1,10 @@
+# Fictionamer
+Written in *Python* by *Randall Arms* @ randallarms.com
+
+## Summary
+Generate names for fictional characters of various genres.
+
+## Instructions
+When prompted, type in the genre you wish to generate a name based on.
+
+> Note: Currently, "fantasy" is the only supported genre. More coming soon!
---
+++ fictionamer.py
@@ -0,0 +1,69 @@
+#======================================#
+#             Fictionamer              #
+#           by Randall Arms            #
+#  github.com/randallarms/fictionamer  #
+#======================================#
+
+from random import randint
+import os
+
+# Opening text
+print("\n\n=========")
+print("Fictionamer")
+print("=========")
+print("Generate a name!")
+
+# Name generation (genres to be added: scifi, horror, steampunk)
+genres = ["fantasy"]
+
+def name_gen(genre):
+
+    if not genre in genres:
+        print("\nGenre not found! ")
+        exit()
+
+    # Determine gender
+    gender = "male"
+    if randint(0,1) == 0:
+        gender = "female"
+    
+    # Get files
+    __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
+    
+    f_prefix = open(os.path.join(__location__, "names/fantasy/prefix.txt"), "r")
+    f_name = open(os.path.join(__location__, "names/fantasy/name_" + gender + ".txt"), "r")
+    f_suffix = open(os.path.join(__location__, "names/fantasy/suffix.txt"), "r")
+    
+    # Fill list of possible names from file
+    prefixes = []
+    names = []
+    suffixes = []
+    for line in f_prefix:
+        prefixes.append(line)
+    for line in f_name:
+        names.append(line)
+    for line in f_suffix:
+        suffixes.append(line)
+
+    # Generate name
+    name_str = ""
+    if randint(0,2) == 0:
+        name_str += prefixes[randint(0, len(prefixes)-1)].strip('\n') + " "
+    name_str += names[randint(0, len(names)-1)].strip('\n')
+    name_str += " " + names[randint(0, len(names)-1)].strip('\n')
+    if randint(0,3) == 0:
+        name_str += ", " + suffixes[randint(0, len(suffixes)-1)].strip('\n')
+    return name_str
+        
+    file.close()
+
+# Get the genre
+print("\nWhich genre do you wish to base the name in? ")
+g = input("> ");
+
+# Generate the name
+name = name_gen(g)
+
+# Print the results
+print("\nGenerated name: ")
+print(name + "\n")
